7a2e6e37e739ab24b058fe99f19858c1315c9241
Vladimir Akopyan <vlad@quickbird.uk>
2015-08-07 16:44:51 +0100

    PUt the right project Here

---
+++ WebWIthIdentity/Models/ApplicationDBCOntext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace WebWIthIdentity.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+
+
+
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
